541dfb0acda7f73e9be8e67f8dbb7187a1a68e56
Erin Wise <erinwise88@gmail.com>
2023-10-18 07:29:35 -0400

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,41 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function handleFilterChange(e) {
+    setFilters(e.target.value)
+  }
+
+  function handleFindPetsClick() {
+      if (filters === "dog") {
+        fetch("http://localhost:3001/pets?type=dog")
+        .then(res => res.json())
+        .then(petData => setPets(petData))
+      } else if (filters === "cat") {
+        fetch("http://localhost:3001/pets?type=cat")
+        .then(res => res.json())
+        .then(petData => setPets(petData))
+      } else if (filters === "micropig") {
+        fetch("http://localhost:3001/pets?type=micropig")
+        .then(res => res.json())
+        .then(petData => setPets(petData))
+      } else {
+        fetch("http://localhost:3001/pets")
+        .then(res => res.json())
+        .then(petData => setPets(petData))
+      }
+    
+  }
+
+  function handleAdoptPet(adoptedPetId) {
+    const updatedPets = pets.map((pet) => {
+      if(pet.id === adoptedPetId) {
+        return {...pet, isAdopted: true}
+      }
+      return pet;
+    });
+    setPets(updatedPets)
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +50,12 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+            onChangeType={handleFilterChange}
+            onFindPetsClick={handleFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet}/>
           </div>
         </div>
       </div>
